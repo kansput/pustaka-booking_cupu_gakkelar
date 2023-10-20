c12ad9562847d3bb6cc07733990715e1886d008d
kansput <kandaputrah@.gmail.com>
2023-10-20 20:48:52 +0700

    pertemuan 5 membuat biodata di about

---
+++ application/views/V_about.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            text-align: center;
+        }
+
+        .container {
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #666;
+        }
+
+        ul {
+            list-style: none;
+            padding: 0;
+        }
+
+        li {
+            margin: 10px 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Biodata</h1>
+        <p>Nama: Kanda Putra Hibatullah</p>
+        <p>Usia: 18 tahun</p>
+        <p>Alamat: Jl.Kayuramin</p>
+        <p>Email: kandaputrah@gmail.com</p>
+        <p>No. Telepon: 9090863634</p>
+        <h2>Pendidikan</h2>
+        <ul>
+            <li>SDS Muhammadiyah 3 Jakarta : 2008-2014</li>
+            <li>SMP Negeri 7 Contoh: 2014-2017</li>
+            <li>SMA Negeri 54 Contoh: 2017-2020</li>
+        </ul>
+       
+    </div>
+</body>
+</html>
---
+++ application/views/V_header.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Web Prog II | Merangcang Templat Sederhana Dengan Codeigniter</title>
+        <link rel="stylesheet" href="<?= base_url()?>assets/css/style.css">
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                <hgroup>
+                    <h1>RentalBuku.net</h1>
+                    <h3>Membuat Tamplate Sederhana Dengan Codeigniter</h3>
+                </hgroup>
+                <nav>
+                    <ul>
+                        <li><a href="<?= base_url().'index.php/web' ?>">Home</a></li>
+                        <li><a href="<?= base_url().'index.php/web/about' ?>">About</a></li>
+                    </ul>
+                </nav>
+                <div class="clear"></div>
+            </header>
